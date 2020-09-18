94352102b53b78b06d0af4b84148d3a68d1e799b
IsaiasEspinal <isaiasespinal19@gmail.com>
2020-09-18 11:34:36 -0400

    Se puede ver un mensaje en home

--- src/server/config.js
+++ src/server/config.js
@@ -1,6 +1,32 @@
-module.exports = app => {
+const express = require('express');
+const path = require('path');
+const exhbs = require('express-handlebars')
+const route = require('../route/index.js');
 
+const morgan = require('morgan');
+const multer = require('multer');
+
+module.exports = app => {
+    //setting
     app.set('port', process.env.PORT || 3000);
+    app.set('views', path.join(__dirname, 'view'));
+    app.engine('.hbs', exhbs({
+        defaultLayout: 'main',
+        partialsDir: path.join(app.get('views'), 'partials'),
+        layoutsDir: path.join(app.get('views'), 'layout'),
+        extname: '.hbs',
+        helpers: require('./helpers')
+    }));
+    app.set('view engine', '.hbs');
+
+    //middelwares
+    app.use(morgan('dev'));
+    app.use(multer({ dest: path.join(__dirname, '../public/upload/temp') }).single('image'));
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
+
+    //routes
+    route(app);
 
     return app;
 }
